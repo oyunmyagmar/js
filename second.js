f0b4af7b327c7dc23f63a79797671b67c6c6752e
oyunmyagmar <25LP4018@nest.edu.mn>
2025-07-07 12:36:45 +0800

    js ehlel

---
+++ second.js
@@ -0,0 +1,23 @@
+//number -> too
+let age = 20;
+
+//string -> ug uguulber
+let name = "miga";
+
+//false -> hudal 0
+//unen -> unen 1
+let graduated = false;
+
+//yer ni bol blohgui
+let num = 10;
+let num1 = "10";
+console.log(num + num1);
+
+//celcuis gradus ugugduug farenheit iig bodoj oloh console.log bichih
+let gradus = 100;
+let farenheit = (100 * 9) / 5 + 32;
+console.log(farenheit);
+
+let gradus2 = 100;
+let far = gradus2 * 1.8 + 32;
+console.log(far);
---
+++ third.js
@@ -0,0 +1,28 @@
+//opearators + - * / %
+
+let num1 = "10";
+let num2 = 10;
+console.log(num1 - num2);
+// deerh bolohgui
+
+let num3 = 40;
+let num4 = 50;
+console.log(num3 + num4);
+
+console.log(num3 - num4);
+
+console.log(num3 * num4);
+
+console.log(num3 / num4);
+
+let remain = 20 % 6;
+console.log(remain);
+
+//assignment operators ni math-iin tentsuu temdeg bish
+let n = 10;
+// neg
+n = 20;
+let n1 = 11;
+let nIsBig = n > n1;
+let nIsBig1 = n >= n1;
+console.log(nIsBig);
