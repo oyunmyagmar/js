3d9adaa7f173674791361a2909d731805f43e1c5
oyunmyagmar <25LP4018@nest.edu.mn>
2025-07-16 02:21:47 +0900

    revised until variables

---
+++ tube.js
@@ -0,0 +1,19 @@
+let n = "mosh";
+console.log(n);
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+let age = 6;
+console.log(age);
+
+let isApproved = false;
+console.log(isApproved);
+
+console.log("Hello World");
+
+let hour = 19;
+if (hour >= 6 && hour < 12) console.log("Good morning!");
+else if (hour >= 12 && hour < 18) console.log("Good afternoon!");
+else console.log("Good evening!");
