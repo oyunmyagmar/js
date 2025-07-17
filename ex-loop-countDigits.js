c54f270291adf46eab2b78d56e48007047486e2e
oyunmyagmar <25LP4018@nest.edu.mn>
2025-07-17 13:41:33 +0900

    first last digits

--- ex-loop-countDigits.js
+++ ex-loop-countDigits.js
@@ -5,13 +5,9 @@ countButton.addEventListener("click", () => {
   let number = parseInt(numberInput.value);
   let i = 0;
 
-  while (i < (number % 10 > 1)) {
+  while (number >= 1) {
     number = number / 10;
     i = i + 1;
-    console.log(number);
   }
-  // if (number % 10 !== 0) {
-  //   number = number / 10;
-  //   console.log(number);
-  // }
+  console.log(i);
 });
