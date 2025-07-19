afb6acf0c8293516c80d8b29b31c41ff12fe5cc1
oyunmyagmar <25LP4018@nest.edu.mn>
2025-07-20 03:36:45 +0900

    stars

--- ex-loop-swapFirstLastDigits.js
+++ ex-loop-swapFirstLastDigits.js
@@ -4,26 +4,29 @@ let swapButton = document.getElementById("swap-btn");
 swapButton.addEventListener("click", () => {
   let number = parseInt(numberInput.value);
   let digit = 0;
-  // let count = 0;
-  // let firstDigit = 0;
-  // let lastDigit = number % 10;
-  // console.log(lastDigit + " is the Last digit");
+  let firstDigit = 0;
+  let lastDigit = 0;
+  let count = 0;
 
-  // while (number >= 1) {
-  //   number = number / 10;
-  //   count = count + 1;
-  // }
-  // console.log(count);
-
-  // firstDigit = Math.floor(number * 10);
-  // console.log(firstDigit + " is the First digit");
-
-  // console.log("after swap " + firstDigit + " is the Last digit");
-  // console.log("after swap " + lastDigit + " is the First digit");
   while (number >= 1) {
     digit = number % 10;
-    console.log(digit);
+    // console.log(digit);
+    firstDigit = digit;
+    console.log(firstDigit);
     number = Math.floor(number / 10);
     // console.log(number);
   }
 });
+// let count = 0;
+// let firstDigit = 0;
+// let lastDigit = number % 10;
+// console.log(lastDigit + " is the Last digit");
+
+// while (number >= 1) {
+//   number = number / 10;
+//   count = count + 1;
+// }
+// console.log(count);
+
+// firstDigit = Math.floor(number * 10);
+// console.log(firstDigit + " is the First digit");
