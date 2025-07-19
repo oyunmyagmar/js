aebae4ce32cdb19dc39847814293eb2846382fd4
oyunmyagmar <25LP4018@nest.edu.mn>
2025-07-19 15:48:16 +0900

    Diagonal done

--- ex-loop-factorials.js
+++ ex-loop-factorials.js
@@ -4,4 +4,13 @@ let calculateButton = document.getElementById("calc-btn");
 calculateButton.addEventListener("click", () => {
   let number = parseInt(numberInput.value);
   let i = 0;
+  let factorial = 1;
+
+  while (i < number) {
+    i = i + 1;
+    factorial = factorial * i;
+
+    console.log(i);
+  }
+  console.log(number + "! is " + factorial);
 });
